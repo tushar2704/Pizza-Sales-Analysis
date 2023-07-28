f43bd9496bc9f451e726d6abcf72eb20b21b855a
tushar2704 <tushar.inseec@gmail.com>
2023-07-29 00:41:16 +0530

    update

--- query/pizza_sales_db.sql
+++ query/pizza_sales_db.sql
@@ -2,3 +2,41 @@
 https://github.com/tushar2704/
 Pizza_Sales_Analysis
 */
+/*---------------------------Creating DB----------------------------------------------------*/
+
+CREATE DATABASE "Pizza_Sales_Analysis"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+----------------------------------------------------------------------------------------------
+
+
+
+/*------------------------------------Table- Pizza-Sales------------------------------------*/
+
+CREATE TABLE pizza_sales (
+	pizza_id SERIAL,
+	order_id INT,
+	pizza_name_id VARCHAR(35),
+	quantity INT,
+	order_date DATE,
+	order_time TIME,
+	unit_price INT,
+	total_price INT,
+	pizza_size VARCHAR(5),
+	pizza_category VARCHAR(35),
+	pizza_ingredients VARCHAR(100),
+	pizza_name VARCHAR(35)
+	);
+
+
+
+
+
+
+
+
+
+

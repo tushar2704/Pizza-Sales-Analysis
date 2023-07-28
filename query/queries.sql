2a1a451552f17e7de4b8ad9338ffdc0d6d5c3365
tushar2704 <tushar.inseec@gmail.com>
2023-07-29 01:51:40 +0530

    DB update

--- query/queries.sql
+++ query/queries.sql
@@ -2,10 +2,43 @@
 @https://github.com/tushar2704/
 Pizza-Sales-Analysis
 */
+SELECT * FROM pizza_sales;
 
+/*------------KPI's------------*/
+--1)Total Revenue
+SELECT
+	SUM(total_price) AS total_revenue
+FROM 
+	pizza_sales;
+	
+--2)Average Order Value
+SELECT
+	SUM(total_price) / COUNT (DISTINCT order_id)  AS AVG_Order_Value
 
+FROM
+	pizza_sales;
+	
+--3)Total Pizza Sold
+SELECT
+	SUM(quantity) AS Total_Pizza_Sold
+FROM
+	pizza_sales;
 
-
+--4)Total Orders
+SELECT
+	COUNT(DISTINCT order_id) AS Total_Orders
+FROM
+	pizza_sales;
+	
+--5)Average Pizzas Per Order
+SELECT
+	ROUND(
+		
+		ROUND(SUM(quantity),2) / ROUND(COUNT(DISTINCT order_id),2)
+		
+		,2) AS Average_pizza_per_order
+FROM
+	pizza_sales;
 
 
 
